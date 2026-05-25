59b30136645e8539483baa32af6b1211aa88906f
kabaleonline <shopkabale@gmail.com>
2026-05-25 11:46:49 +0300

    Create src/lib/static/categories.ts

---
+++ src/lib/static/categories.ts
@@ -0,0 +1,55 @@
+export const futureCategories = [
+  { 
+    name: "Laptops", 
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/laptops" 
+  },
+  { 
+    name: "Computer Parts", 
+    image: "https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/computer-parts" 
+  },
+  { 
+    name: "Smartphones", 
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/smartphones" 
+  },
+  { 
+    name: "Enterprise", 
+    image: "https://images.unsplash.com/photo-1558494949-ef010cbdcc31?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/networking" 
+  },
+  { 
+    name: "Tablets", 
+    image: "https://images.unsplash.com/photo-1544244015-0df4b3dfc5ce?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/tablets" 
+  },
+  { 
+    name: "Storage", 
+    image: "https://images.unsplash.com/photo-1597839219467-1b3b27b3b44b?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/storage" 
+  },
+];
+
+export const trendingCategories = [
+  { 
+    name: "Tech", 
+    image: "https://images.unsplash.com/photo-1498049794561-7780e7231661?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/tech" 
+  },
+  { 
+    name: "Accessories", 
+    image: "https://images.unsplash.com/photo-1572569431708-36423c8a9844?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/accessories" 
+  },
+  { 
+    name: "Audio", 
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/audio" 
+  },
+  { 
+    name: "Gaming", 
+    image: "https://images.unsplash.com/photo-1600080972464-8e5f35f63d08?auto=format&fit=crop&w=400&q=80", 
+    href: "/category/gaming" 
+  },
+];
